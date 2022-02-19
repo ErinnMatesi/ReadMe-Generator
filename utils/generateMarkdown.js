31dc8bdb2297bec88bee2fe3cb139b8927b4e667
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-02-19 13:25:50 -0800

    altered licensebadge to use licenselink but still getting full function printed out

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,17 +2,18 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   renderLicenseLink(license);
-  if (license == Apache) {
-    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-  } else if (license == BSD) {
-    return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
-  } else if (license == MIT) {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-  } else if (license == Mozilla) {
-    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-  } else {
-    return
-  }
+  return `[![License](${renderLicenseLink}](https://opensource.org/licenses/Apache-2.0)`
+  // if (license == Apache) {
+  //   return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  // } else if (license == BSD) {
+  //   return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  // } else if (license == MIT) {
+  //   return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  // } else if (license == Mozilla) {
+  //   return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  // } else {
+  //   return
+  // }
 };
 // use this: https://gist.github.com/qvil/5e3ed56c26d784e51424621119cc4028
 
@@ -57,8 +58,6 @@ function generateMarkdown(data) {
   
   # License
   ${renderLicenseBadge}
-  ${renderLicenseLink}
-  ${renderLicenseSection}
   
   ---
   #Contact me
