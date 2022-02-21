040b6233118ead34fe5d1790ee26930b6410dea9
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-02-21 14:34:13 -0800

    fixed issues with license link rendering properly. Added table of contents and changed Contact section to Questions to meet acceptance criteria

--- index.js
+++ index.js
@@ -10,43 +10,43 @@ const questions = [
         message: 'What is the application name?',
         name: 'name'
     },
-    // {
-    //     type: 'input',
-    //     message: 'Tell me about your application',
-    //     name: 'description'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What are the installation instructions?',
-    //     name: 'installation'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'How do I use the application?',
-    //     name: 'usage'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What are the guidelines for contributing to the application?',
-    //     name: 'contributions'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What are the test instructions for the application?',
-    //     name: 'test'
-    // },
-    // {
-    //     type: 'list',
-    //     message: 'What type of license will you use?',
-    //     name: 'license',
-    //     choices: ['Apache', 'BSD', 'MIT', 'Mozilla'],
-    //     default: 'MIT'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'What is your e-mail address?',
-    //     name: 'email'
-    // },
+    {
+        type: 'input',
+        message: 'Tell me about your application',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'How do I use the application?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What are the guidelines for contributing to the application?',
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for the application?',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'What type of license will you use?',
+        name: 'license',
+        choices: ['Apache', 'BSD', 'MIT', 'Mozilla', 'none'],
+        default: 'MIT'
+    },
+    {
+        type: 'input',
+        message: 'What is your e-mail address?',
+        name: 'email'
+    },
     {
         type: 'input',
         message: 'What is your GitHub username?',
@@ -54,19 +54,21 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-inquirer.prompt(questions)
-.then ((data) => {
+function init() {
+    inquirer.prompt(questions)
+    .then ((data) => {
     const generateContent = generateMarkdown(data);
     fs.writeFile('./utils/ReadMe.md', generateContent, (error) => 
         error ? console.log(err) : console.log('Created ReadMe.md')
     )
 });
+};
+
+// Function call to initialize app
+init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,68 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  renderLicenseLink(license);
-  return `[![License](${renderLicenseLink}](https://opensource.org/licenses/Apache-2.0)`
-  // if (license == Apache) {
-  //   return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-  // } else if (license == BSD) {
-  //   return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
-  // } else if (license == MIT) {
-  //   return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-  // } else if (license == Mozilla) {
-  //   return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-  // } else {
-  //   return
-  // }
+  // renderLicenseLink(license);
+  // return `[![License](${renderLicenseLink}](https://opensource.org/licenses/Apache-2.0)`
+  console.log(license);
+  if (license === 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === 'BSD') {
+    return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  } else if (license === 'MIT') {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (license === 'Mozilla') {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  } else {
+    return ""
+  }
 };
 // use this: https://gist.github.com/qvil/5e3ed56c26d784e51424621119cc4028
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license == Apache) {
-    return "https://img.shields.io/badge/License-Apache%202.0-blue.svg"
-  } else if (license == BSD) {
-    return "https://img.shields.io/badge/License-BSD%203--Clause-blue.svg"
-  } else if (license == MIT) {
-    return "https://img.shields.io/badge/License-MIT-yellow.svg"
-  } else if (license == Mozilla) {
-    return "https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg"
+  if (license === 'none') {
+    return ""
   } else {
-    return
+    return `* [License](#license)`
   }
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'none') {
+    return ""
+  } else {
+    return `# License
+${renderLicenseBadge(license)}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.name}
 
-  ## Description
-  ${data.description}
-  
-  ## Installation
-  ${data.installation}
-  
-  ## Usage
-  ${data.usage}
-  
-  ## Contributions
-  ${data.contributions}
-  
-  ## Testing
-  ${data.test}
-  
-  # License
-  ${renderLicenseBadge}
-  
-  ---
-  #Contact me
-  ${data.email}
-  [${data.github}](https://github.com/${data.github})
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Testing](#testing)
+* [Questions](#questions)
+${renderLicenseLink(data.license)}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributions
+${data.contributions}
+
+## Testing
+${data.test}
+
+${renderLicenseSection(data.license)}
+
+---
+#Questions
+${data.email}
+[${data.github}](https://github.com/${data.github})
 `;
 };
 
